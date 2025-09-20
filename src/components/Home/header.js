32ebe18c0bcf922d483303301243b410468ebb72
Hazama32 <bintanghack32@gmail.com>
2025-09-20 11:56:34 +0700

    first commit

--- src/components/Home/header.js
+++ src/components/Home/header.js
@@ -7,7 +7,7 @@ const Header = () => {
   return (
     <Fade duration={2200}>
       <div className="max-w-7xl mx-auto lg:px-6 md:px-3">
-        <div className="my-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-0 xl:mt-28 flex gap-10 lg:flex-justify lg:flex flex-col lg:flex-row flex-col-reverse">
+        <div className="my-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-0 xl:mt-28 flex gap-10 lg:flex-justify lg:flex lg:flex-row flex-col-reverse">
           <div className="text-center lg:text-left flex flex-col justify-center">
             <h1 className="text-black text-4xl font-semibold opacity-70">
               MY NAME IS HOLO
